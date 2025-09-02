839de2ec114479f327cb7dac0bd1813dbfd90844
Luís Corrêa <lelctf@hotmail.com>
2025-09-02 16:13:44 +0000

    migrations improvements

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,8 +3,11 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  if (!["GET", "POST"].includes(request.method)) {
-    return response.status(405).end(`Method ${request.method} Not Allowed`);
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response
+      .status(405)
+      .json({ error: `Method ${request.method} Not Allowed` });
   }
 
   let dbClient;
@@ -36,11 +39,10 @@ export default async function migrations(request, response) {
 
       return response.status(200).json(migratedMigrations);
     }
-  } catch (e) {
-    return response.status(500).json(e.message);
+  } catch (error) {
+    console.error(error);
+    throw error;
   } finally {
     await dbClient.end();
   }
-
-  return response.status(405).end();
 }
